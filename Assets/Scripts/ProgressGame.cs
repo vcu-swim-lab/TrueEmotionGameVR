1619e75023c6c37af12ffbe6c9b8727226c0c70a
Ngoc Nguyen <nguyengoc121203@gmail.com>
2025-10-29 11:31:42 -0400

    added scenario for each emotion

--- Assets/Scripts/ProgressGame.cs
+++ Assets/Scripts/ProgressGame.cs
@@ -8,14 +8,13 @@ using UnityEngine.Android;
 using Random = UnityEngine.Random;
 
 
-[RequireComponent(typeof(EmotionPredictor))]
-[RequireComponent(typeof(OVRFaceExpressions))]
+[RequireComponent(typeof(FaceAuModel))]
 public class ProgressGame : MonoBehaviour
 {
     private TextMeshProUGUI text;
     // private TextMeshProUGUI debug;
 
-    private EmotionPredictor predictor;
+    private FaceAuModel predictor;
 
     [SerializeField]
     private ModelAsset soundModelTemp;
@@ -31,7 +30,19 @@ public class ProgressGame : MonoBehaviour
         { Emotion.Surprise, "😲" },
     };
 
+    // Map emotion name to scenario to display to user
+    private static readonly Dictionary<Emotion, string> emotionToScenario = new()
+    {
+        { Emotion.Anger, "You just found out someone pump into your car in the parking lot and did not leave a note." },
+        { Emotion.Disgust, "You saw a plate of spoiled food on the table with maggots crawling on it." },
+        { Emotion.Fear, "You feel a sudden chill as you hear footsteps behind you in a dark alley." },
+        { Emotion.Happiness, "You are watching your favorite band perform live at a concert for the first time." },
+        { Emotion.Sadness, "You just watched a touching movie where the main character separated from their animal best friend." },
+        { Emotion.Surprise, "You found out a childhood friend is coming to visit!" },
+    };
+
     private readonly Emotion[] emotionList = emotionToEmoji.Keys.ToArray();
+    private readonly Emotion[] scenarioEmotionList = emotionToScenario.Keys.ToArray();
 
 
     void Start()
@@ -39,7 +50,7 @@ public class ProgressGame : MonoBehaviour
         text = GameObject.Find("Instruction").GetComponent<TextMeshProUGUI>();
         // debug = GameObject.Find("Debug").GetComponent<TextMeshProUGUI>();
 
-        predictor = GetComponent<EmotionPredictor>();
+        predictor = GetComponent<FaceAuModel>();
 
         RunGame();
         RunSoundTest();
@@ -93,6 +104,29 @@ public class ProgressGame : MonoBehaviour
                 text.text = $"Score: {this_score}/10";
                 await Awaitable.WaitForSecondsAsync(1f);
             }
+            // Scenario round
+            text.text = "This is the scenario round. You have 10s to act each scenario shown to you. Good luck.";
+            await Awaitable.WaitForSecondsAsync(2f);
+
+            foreach (var emotion in scenarioEmotionList)
+            {
+             for (int j = 3; j >= 0; --j)
+                {
+                    text.text = $"{j}";
+                    await Awaitable.WaitForSecondsAsync(1f);
+                }
+
+                // Show scenario for current emotion
+                string scenario = emotionToScenario[emotion];
+                text.text = $"{scenario}\n({emotion})";
+
+                // Run prediction loop for this emotion
+                int this_score = await RunPredictionCoroutine(emotion);
+                score[emotion] += this_score; // accumulate score
+
+                text.text = $"Score: {this_score}/10";
+                await Awaitable.WaitForSecondsAsync(1f);
+            }
 
             text.text = "Thanks for playing! Scores:\n" +
                         string.Join("\n", score.Select(kv => $"{kv.Key}: {kv.Value}/10")) + "\n\n" +
@@ -111,9 +145,6 @@ public class ProgressGame : MonoBehaviour
 
     private async Awaitable<int> RunPredictionCoroutine(Emotion emotion)
     {
-        predictor.Flush(); // Ensure we start with fresh data
-        predictor.Polling = true;
-
         int score = 0;
 
         const int intervalMs = 1000;
@@ -147,8 +178,6 @@ public class ProgressGame : MonoBehaviour
         }
 
         print("Done with predicting " + emotion);
-
-        predictor.Polling = false;
 
         return score;
     }
